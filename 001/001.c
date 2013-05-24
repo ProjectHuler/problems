d8abeed18621be89f7439ad3ebf5d9eb3c25bfb4
hanhwu <hanhwu@yahoo.com>
2013-05-24 09:34:12 -0600

    Overwritten 001 C with elegant solution

--- 001/001.c
+++ 001/001.c
@@ -1,13 +1,32 @@
-#include <stdio.h>
+/***************
+Problem 001
+Find the sum of all the multiples of 3 or 5 below 1000.
+***************/
 
-int main(void) {
-    int sum=0;
-    // Loop through integers 1 to 999 looking for integers that are divisible
-    // by 3 or 5. If they are, then add to the running sum.
-    for(int i=1; i<1000; i++) {
-        if(i%3 == 0 || i%5 == 0)
-            sum += i;
-    }
-    printf("%i\n", sum);
-    return 0;
+#include<stdio.h>
+#include<math.h>
+
+int solve001( int a, int b, int limit );
+
+void main()
+{
+    int p1 = 3;
+	int p2 = 5;
+	int p3 = 1000;
+	int ans = solve001(p1, p2, p3-1);
+	printf("answer=%i\n",ans);
+}
+
+int solve001( int a, int b, int limit )
+{
+	/**********
+	Method:	(3,6,9...999) + (5,10,15...995) - (15,30,45...)
+	Separated into three lines for readability:
+	(3+6+9+...+999) = 3(1+2+3...333) = 3(1+333)(333/2)
+	**********/
+	int solve001=0;
+	solve001 += a*(1+floor(((float)limit)/a))*floor(((float)limit)/a)/2;
+	solve001 += b*(1+floor(((float)limit)/b))*floor(((float)limit)/b)/2;
+	solve001 -= (a*b)*(1+floor(((float)limit)/(a*b)))*floor(((float)limit)/(a*b))/2;
+	return solve001;
 }
