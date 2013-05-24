663dc7d375aee22b5ae8a7179a2fb08d3f801324
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2013-05-23 19:34:27 -0500

    Combined conditional statement in if.
    Combined the if - else if block into a single if statement.

--- 001/001.c
+++ 001/001.c
@@ -5,12 +5,8 @@ int main(void) {
     // Loop through integers 1 to 999 looking for integers that are divisible
     // by 3 or 5. If they are, then add to the running sum.
     for(int i=1; i<1000; i++) {
-        if(i%3 == 0) {
+        if(i%3 == 0 || i%5 == 0)
             sum += i;
-        }
-        else if(i%5 == 0) {
-            sum += i;
-        }
     }
     printf("%i\n", sum);
     return 0;
