8a78a9b38fa4ce9cdd646741dea297c6027c13e2
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2013-05-23 21:01:41 -0500

    More elegant solution 001.py solution.

--- 001/001.py
+++ 001/001.py
@@ -1,11 +1,46 @@
-"""
-Project Euler Problem 1:
+import numpy as np
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we
-get 3, 5, 6 and 9. The sum of these multiples is 23.
+def sum_of_multiples(multiples, max_value):
+    """Takes a length 2 list of multiples to check, and an integer maximum
+    value to check."""
+    # To elegantly solve this problem we are going to find the sum of all the
+    # numbers that are divisible by the first number in the list "multiple",
+    # and then the second number in the list "multiple". In the process we have
+    # over counted by all the numbers that are divisible by the two "multiple"
+    # numbers multiplied together. So we subtract out the sum of all the
+    # numbers that are divisible by the two "multiple" numbers multiplied
+    # together.
+    #
+    # Explicitly for this problem, we find the sum of numbers divisible by 3,
+    # add that to the sum of numbers divisible by 5, and subtract the sum of
+    # numbers divisible by 15.
+    if len(multiples) != 2:
+        print "Expected only two multiples to check."
+        print "Out of scope, I'll need additional funds for a scope change."
+        return None
+    multiples.append(multiples[0]*multiples[1])
+    # Make a numpy array for easier math.
+    multiples = np.array(multiples)
 
-Find the sum of all the multiples of 3 or 5 below 1000.
-"""
-# List comprehention should be faster than Python for-loop.
-# range(1,1000) returns a list from 1 to 999.
-print sum([i for i in range(1,1000) if (i%3==0 or i%5==0)])
+    # The sum of numbers divisible by a number can be calculated by taking the
+    # number itself and adding it to the largest number less than max_value
+    # that it is divisible by, and then multiplying it by the half the number
+    # of numbers that it is divisible by within the range.
+    #
+    # Explicitly for this problem for the number 5, there are 999/5 = 199
+    # numbers that are divisible by 5 between 1 to 999. The first number is 5
+    # and the last number is 999-999%5 = 995. The sum of the first and last
+    # number is 1000. The sum of the second number and the second to last
+    # number also add to 1000, and so on. Of the 199 numbers that are divisible
+    # by 5, there are 199/2.0 of these pairs, and the sum of all these is
+    # 1000(199/2.0). NOTE: we divide by a float in case the number of numbers
+    # divisible by a number is odd.
+
+    sub_totals = (multiples+max_value-max_value%multiples)*\
+            (max_value/multiples)/2.0
+
+    # Sum the first two numbers and subtract the last number to get the total.
+    return sum(sub_totals[0:2]) - sub_totals[2]
+
+if __name__ == "__main__":
+    print sum_of_multiples(multiples=[3,5], max_value=999)
