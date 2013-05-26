728bd7edd33f504cfa92462764e0ad11fef3f9ee
Han Hwu <hanhwu@yahoo.com>
2013-05-25 20:46:23 -0700

    Added C solution to 002
    Recursion used.
    Non-recursion also included, but commented out.

---
+++ 002/002.c
@@ -0,0 +1,53 @@
+int solve002( int a, int b, int limit, int answer);
+
+int main(void)
+{
+	int p1 = 1;
+	int p2 = 2;
+	int p3 = 4000000;
+	int answer = 0;
+
+	if( p1%2 == 0 )
+	{ answer += p1;	}
+	if( p2%2 == 0 )
+	{ answer += p2;	}
+
+	answer = solve002( p1,p2,p3,answer );
+	printf("%i\n",answer);
+}
+
+int solve002( int a, int b, int limit, int sum )
+{
+	int c = a + b;
+	if( c < limit )
+	{
+		sum = solve002( b,c,limit,sum );
+		if( c%2 == 0 )
+		{
+			sum += c;
+		}
+	}
+	return sum;
+}
+
+/**************
+
+while method, no recursion	
+
+int solve002( int a, int b, int limit, int sum )
+{
+	int c = a + b;
+	while( c < limit )
+	{
+		if( c%2 == 0 )
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+		c = a+b;
+	}
+	return sum;
+}
+
+**************/
