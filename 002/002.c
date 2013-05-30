68e34081653d77e0271ec625c383cac7e1f27331
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2013-05-29 20:16:03 -0500

    Cleaned up C code.

--- 002/002.c
+++ 002/002.c
@@ -6,14 +6,12 @@ int main(void) {
 	int p1 = 1;
 	int p2 = 2;
 	int p3 = 4000000;
-	int answer = 0;
+    // Start off the answer at 2.
+	int answer = 2;
 
-	if( p1%2 == 0 ) {
-        answer += p1;	}
-	if( p2%2 == 0 ) {
-        answer += p2;	}
-
-	answer = solve002( p1,p2,p3,answer );
+    // Recursively call the function solve002 to generate the next entry in the
+    // Fibonacci sequence
+    answer = solve002( p1,p2,p3,answer );
 	printf("%i\n",answer);
 }
 
@@ -21,31 +19,10 @@ int solve002( int a, int b, int limit, int sum ) {
 	int c = a + b;
 	if( c < limit ) {
 		sum = solve002( b,c,limit,sum );
+        // Test for even-ness, and add to the answer if it is even.
 		if( c%2 == 0 ) {
 			sum += c;
 		}
 	}
 	return sum;
 }
-
-/**************
-
-while method, no recursion
-
-int solve002( int a, int b, int limit, int sum )
-{
-	int c = a + b;
-	while( c < limit )
-	{
-		if( c%2 == 0 )
-		{
-			sum += c;
-		}
-		a = b;
-		b = c;
-		c = a+b;
-	}
-	return sum;
-}
-
-**************/
