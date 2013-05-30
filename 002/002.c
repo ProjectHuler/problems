5e4e21809e39d09b789ddb93b4578571d163a873
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2013-05-29 19:37:31 -0500

    Cleaned up some whitespace and bracket issues.

--- 002/002.c
+++ 002/002.c
@@ -2,30 +2,26 @@
 
 int solve002( int a, int b, int limit, int answer);
 
-int main(void)
-{
+int main(void) {
 	int p1 = 1;
 	int p2 = 2;
 	int p3 = 4000000;
 	int answer = 0;
 
-	if( p1%2 == 0 )
-	{ answer += p1;	}
-	if( p2%2 == 0 )
-	{ answer += p2;	}
+	if( p1%2 == 0 ) {
+        answer += p1;	}
+	if( p2%2 == 0 ) {
+        answer += p2;	}
 
 	answer = solve002( p1,p2,p3,answer );
 	printf("%i\n",answer);
 }
 
-int solve002( int a, int b, int limit, int sum )
-{
+int solve002( int a, int b, int limit, int sum ) {
 	int c = a + b;
-	if( c < limit )
-	{
+	if( c < limit ) {
 		sum = solve002( b,c,limit,sum );
-		if( c%2 == 0 )
-		{
+		if( c%2 == 0 ) {
 			sum += c;
 		}
 	}
@@ -34,7 +30,7 @@ int solve002( int a, int b, int limit, int sum )
 
 /**************
 
-while method, no recursion	
+while method, no recursion
 
 int solve002( int a, int b, int limit, int sum )
 {
